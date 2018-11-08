857fd6b0ccaa5cb0d14066e03a35ba20f2a920ab
Murilo Kakazu <yuri.kakazu@gmail.com>
2018-11-07 22:34:18 -0200

    added more methods to structures

--- AutomataCLI/AutomataCLI/Struct/Automata.cs
+++ AutomataCLI/AutomataCLI/Struct/Automata.cs
@@ -5,12 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AutomataCLI.Struct {
-    class Automata {
+    public class Automata {
         public AutomataType Type { get; protected set; }
         public List<State> States { get; protected set; }
         public List<Transition> Transitions { get; protected set; }
         public State InitialState { get; protected set; }
         public List<State> FinalStates { get; protected set; }
+
+        public Automata() {
+            States      = new List<State>();
+            Transitions = new List<Transition>();
+            FinalStates = new List<State>();
+        }
 
         public void AddState(State state) {
             this.States.Add(state);
@@ -24,6 +30,7 @@ namespace AutomataCLI.Struct {
 
         public void SetStates(ICollection<State> stateCollection) {
             States = new List<State>(stateCollection);
+            this.RefreshFinalStates();
         }
 
         public void SetInitialState(State state) {
@@ -32,12 +39,35 @@ namespace AutomataCLI.Struct {
 
         private void RefreshFinalStates() {
             if (States != null) {
-                this.FinalStates = States.Where(x => x.IsFinal);
+                this.FinalStates = States.Where(
+                    x => x.IsFinal
+                ).ToList();
             }
         }
 
         public void ClearStates() {
             this.States.Clear();
+        }
+
+        public override String ToString() {
+            String newLine      = Environment.NewLine;
+            String type         = this.Type.ToString() + newLine;
+            String states       = "";
+            String initialState = this.InitialState.ToString() + newLine;
+            String finalStates  = "";
+            String transitions  = "";
+
+            this.States.ForEach(
+                x => states += x.ToString() + newLine
+            );
+            this.FinalStates.ForEach(
+                x => finalStates += x.ToString() + newLine
+            );
+            this.Transitions.ForEach(
+                x => transitions += x.ToString() + newLine
+            );
+
+            return $"{type}{states}{initialState}{finalStates}{transitions}";
         }
     }
 }
